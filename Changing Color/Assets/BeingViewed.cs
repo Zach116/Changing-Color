5773c2c8d1c3577f193a52c4281d51384320414c
Zach116 <zach.bishop116@gmail.com>
2017-11-20 14:11:47 -0800

    Update BeingViewed.cs
    Changed from using collider to using mesh

--- Changing Color/Assets/BeingViewed.cs
+++ Changing Color/Assets/BeingViewed.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class BeingViewed : MonoBehaviour {
 	private List<GameObject> allObjects = new List<GameObject>();
-	private Collider anObjCollider;
+	private Renderer anObjRenderer;
 	private Camera cam;
 	private Plane[] planes;
 	public int timeUntilChange = 5;
@@ -15,6 +15,8 @@ public class BeingViewed : MonoBehaviour {
 		}
 		allObjects.Remove (GameObject.Find ("Main Camera"));
 		allObjects.Remove (GameObject.Find ("Directional Light"));
+		allObjects.Remove (GameObject.Find ("Sun Study"));
+
 
 		cam = Camera.main;
 		planes = GeometryUtility.CalculateFrustumPlanes (cam);
@@ -26,20 +28,24 @@ public class BeingViewed : MonoBehaviour {
 
 		//Go through every object and see if the camera is looking at it
 		for (int i = 0; i < allObjects.Count; i++){
-			//Assign current object's collider to a variable
-			anObjCollider = allObjects[i].GetComponent<Collider>();
+			//Assign current object's renderer to a variable
+			anObjRenderer = allObjects[i].GetComponent<Renderer>();
+
 
 			//Detect if object is in the view of the camera
-			if (GeometryUtility.TestPlanesAABB (planes, anObjCollider.bounds) && ((allObjects[i].GetComponent<Count>().count % timeUntilChange) == 0)) {
-				//Change color of the object that is being looked at
-				allObjects[i].GetComponent<Renderer>().material.color = Random.ColorHSV();
-
+			if (GeometryUtility.TestPlanesAABB (planes, anObjRenderer.bounds)) {
 				//Add one to the count if the object has a count script variable
 				if (allObjects[i].GetComponent<Count> () != null) {
 					allObjects[i].GetComponent<Count> ().AddOneCount ();
 				}
 
-				//Print info when space is clicked
+				//When the object has been looked at for a set amount of time, do something
+				if ((allObjects[i].GetComponent<Count> ().count % timeUntilChange) == 0) {
+					//Change color of the object that is being looked at
+					allObjects[i].GetComponent<Renderer> ().material.color = Random.ColorHSV ();
+				}
+
+				//Print info when left control is clicked
 				if (Input.GetKeyDown (KeyCode.LeftControl)) {
 					Debug.Log (allObjects[i].name + " is being looked at");
 					Debug.Log (allObjects[i].name + " has been looked at for " + allObjects [i].GetComponent<Count>().count + " seconds");
